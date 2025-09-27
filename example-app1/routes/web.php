3df6b8fdc07bf943ad76d3977ed672ee99ee164d
N00240212JodieKane <N00240212@iadt.ie>
2025-09-27 21:23:32 +0100

    home.blade.php

--- example-app1/routes/web.php
+++ example-app1/routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
+
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $posts = [];
+    if (auth()->check()) {
+     $posts = auth()->user()->userCoolPosts()->latest()->get();
+    }   
+    return view('home', ['posts' => $posts]);
 });
+
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/logout', [UserController::class, 'logout']);
+Route::post('/login', [UserController::class, 'login']);
+
+//Blog Post Route
+
+Route::post('/create-post', [PostController::class, 'createPost']);
+Route::get('/edit-post/{post}', [PostController::class, 'showEditScreen']);
+Route::put('/edit-post/{post}', [PostController::class, 'actuallyUpdatePost']);
+Route::delete('/delete-post/{post}', [PostController::class, 'deletePost']);
